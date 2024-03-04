7d31d4bd3d661988ff556bb4fc885e684afc0e4c
lsljw65 <48504421+lsljw65@users.noreply.github.com>
2024-03-04 18:07:17 +0900

    1'st

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import Day from './component/Day';
+import DayList from './component/DayList';
+import EmptyPage from './component/EmptyPage';
+import Header from './component/Header';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+function App() {
+  
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<DayList/>}/>
+          <Route path="/day/:day" element={<Day/>}/>
+          <Route path="/*" element={<EmptyPage/>}/>
+        </Routes>
+        
+      </div>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
