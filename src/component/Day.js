8a5db1a43c9999afa5a484b4dc4956621f37e6cf
lsljw65 <48504421+lsljw65@users.noreply.github.com>
2024-03-05 13:22:33 +0900

    강의 14chapter

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,27 +1,24 @@
-import dummy from "../db/data.json";
+// import dummy from "../db/data.json";
 import{useParams} from "react-router-dom";
+import Word from "./Word";
+// import { useEffect, useState } from "react";
+import useFetch from "../hooks/userFetch";
 
 export default function Day(){
     // dummy.words
     const {day}=useParams();
-    const wordList=dummy.words.filter(word=>(
+    /* const wordList=dummy.words.filter(word=>(
         word.day===Number(day)
-    ))
+    )) */
     
+    const words=useFetch(`http://localhost:3001/words?day=${day}`)
     return (
         <>
             <h2>Day {day}</h2>
             <table>
                 <tbody>
-                    {wordList.map(word=>(
-                        <tr key={word.id}>
-                            <td>
-                                {word.eng}
-                            </td>
-                            <td>
-                                {word.kor}
-                            </td>
-                        </tr>    
+                    {words.map(word=>(
+                         <Word word={word} key={word.id}/> 
                     ))}
                     
                 </tbody>
