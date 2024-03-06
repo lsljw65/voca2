d564fa7b6152bbdf1f092db247ba4bf6c56818ab
lsljw65 <48504421+lsljw65@users.noreply.github.com>
2024-03-06 12:48:37 +0900

    chapter15
    15강까지 학습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import CreateDay from './component/CreateDay';
+import CreateWord from './component/CreateWord';
 import Day from './component/Day';
 import DayList from './component/DayList';
 import EmptyPage from './component/EmptyPage';
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList/>}/>
           <Route path="/day/:day" element={<Day/>}/>
+          <Route path="/create_word" element={<CreateWord/>} />
+          <Route path="/create_day" element={<CreateDay/>} />
           <Route path="/*" element={<EmptyPage/>}/>
         </Routes>
         
